9932571bd9e7b602d3d2330b82ca393a9d7e7f40
cash <1594466572@qq.com>
2020-04-13 18:28:47 +0800

    fix

---
+++ deployserver/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "io"
+  "net/http"
+  "os/exec"
+  "log"
+)
+
+func firstPage(w http.ResponseWriter, r *http.Request) {
+  io.WriteString(w, "<h1> Hello, this is my first page!</h1>")
+}
+
+func main() {
+  http.HandleFunc("/", firstPage)
+  http.ListenAndServe(":8011", nil)
+}
